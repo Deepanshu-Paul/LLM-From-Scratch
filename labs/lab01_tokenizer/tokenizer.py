3bc4e172bef5a9957d758f0e2d621ab0460a64e7
Deepanshu-Paul <deepanshu.paul555@gmail.com>
2026-07-21 12:09:16 +0530

    feat: initialize lab

---
+++ labs/lab01_tokenizer/tokenizer.py
@@ -0,0 +1,10 @@
+class CharacterTokenizer:
+    """
+    A simple character-level tokenizer.
+
+    This tokenizer learns a vocabulary of unique characters
+    and provides methods to encode text into integer IDs
+    and decode IDs back into text.
+    """
+    def __init__(self):
+        pass
